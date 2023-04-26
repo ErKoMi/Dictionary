1be5de2c2747dbc8e242e0a91afd7e1a8da40c47
ErKoMi <konst_ermakov03@mail.ru>
2023-04-27 00:51:12 +0300

    Реализовал загрузку и сохранение

--- src/main/java/org/example/Dictionary/Dictionary.java
+++ src/main/java/org/example/Dictionary/Dictionary.java
@@ -1,27 +1,68 @@
 package org.example.Dictionary;
 
+import java.io.*;
 import java.util.HashMap;
+import java.util.IllegalFormatException;
+import java.util.Map;
 
 public abstract class Dictionary {
     HashMap<String, String> dictionary;
-
+    protected int keyIndex;
+    protected static final String splitter = "-";
     public Dictionary(){
         dictionary = new HashMap<>();
+        keyIndex = 0;
     }
 
-    public abstract void loadFromFile(String fileName);
-    public abstract void saveToFile(String fileName);
-    public void add(String key, String value) throws IllegalArgumentException{
-        if(check(key)){
-            dictionary.put(key, value);
+    public final void loadFromFile(String filePath) throws IOException{
+        File file = new File(filePath);
+        if(!file.exists()){
+            throw new FileNotFoundException("File doesn't exist!");
         }
-        else {
-            throw new IllegalArgumentException("Invalid key value!");
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            for(String string = reader.readLine(); string != null; string = reader.readLine()) {
+                String[] pair = string.split(splitter);
+                if (pair.length != 2) {
+                    throw new IOException("Illegal row!");
+                }
+
+                add(pair[keyIndex], pair[1 - keyIndex]);
+            }
         }
     }
+
+    public final void saveToFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        if(!file.exists()){
+            throw new FileNotFoundException("File doesn't exist!");
+        }
+
+        try(FileWriter writer = new FileWriter(file)){
+            String string;
+            for (Map.Entry<String, String> entry: dictionary.entrySet()){
+                string = keyIndex == 0? entry.getKey() + splitter + entry.getValue():entry.getValue() + splitter + entry.getKey();
+                writer.write(string);
+            }
+        }
+    }
+
+    public void add(String key, String value) throws IllegalArgumentException{
+        if (!checkKey(key)) {
+            throw new IllegalArgumentException("Invalid key!");
+        }
+
+        if (!checkValue(value)) {
+            throw new IllegalArgumentException("Invalid value!");
+        }
+
+        dictionary.put(key, value);
+    }
+
     public String get(String key){
         return dictionary.get(key);
     }
 
-    protected abstract boolean check(String key);
+    protected abstract boolean checkKey(String key);
+    protected abstract boolean checkValue(String value);
 }
---
+++ src/main/java/org/example/Dictionary/LatinNumberDictionary.java
@@ -0,0 +1,2 @@
+package org.example.Dictionary;public class LatinNumberDictionary {
+}
