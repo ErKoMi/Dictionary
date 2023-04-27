8283dfcac6c40eb0d3a3baa4de10305089501018
ErKoMi <konst_ermakov03@mail.ru>
2023-04-27 09:40:01 +0300

    Реализовал Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,74 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.example.Dictionary.Dictionary;
+import org.example.Dictionary.LatinNumberDictionary;
+import org.example.Dictionary.NumberLatinDictionary;
+import org.example.menu.Menu;
+import org.example.menu.MenuItem;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Main {
+    static Dictionary dictionary;
+    static final String fileName = "dictionary.txt";
+    static String filePath;
+    static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        filePath = Path.of(fileName).toAbsolutePath().toString();
+        File file = new File(filePath);
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            out.println("Не удалось создать файл для словаря!");
+        }
+
+        menu.addItem("num", new MenuItem("Выбрать словарь, где ключ - цифры", () ->{
+            dictionary = new NumberLatinDictionary();
+            loadDictionary();
+        },
+        () -> true));
+
+        menu.addItem("latin", new MenuItem("Выбрать словарь, где ключ - латинские символы", () ->{
+            dictionary = new LatinNumberDictionary();
+            loadDictionary();
+        },
+        () -> true));
+
+        menu.addItem("show", new MenuItem("Искать по ключу.", () -> {
+            String key = in.nextLine();
+            String value = dictionary.get(key);
+            out.println(value);
+        },
+        () -> dictionary != null));
+
+        menu.addItem("add", new MenuItem("Добавить значение.", () -> {
+            out.println("Введите ключ значение через пробел:");
+            String[] pair = in.nextLine().split(" ");
+            if(pair.length != 2){
+                out.println("Неверный формат ввода!");
+            }
+
+            dictionary.add(pair[0], pair[1]);
+        },
+        () -> dictionary != null));
+
+        menu.start();
+    }
+
+    private static void loadDictionary() {
+        try {
+            dictionary.loadFromFile(filePath);
+        } catch (IOException e) {
+            out.println("Не удалось загрузить словарь!");
+        }
     }
 }
